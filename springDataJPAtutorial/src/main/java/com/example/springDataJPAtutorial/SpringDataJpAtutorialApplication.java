5b8f754d77a51fba0ea681786d68591bce06fa03
Manish190502 <saimanishreddy92@gmail.com>
2025-01-20 23:15:17 +0530

    first commit of spring data JPA

---
+++ springDataJPAtutorial/src/main/java/com/example/springDataJPAtutorial/SpringDataJpAtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springDataJPAtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpAtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpAtutorialApplication.class, args);
+	}
+
+}
---
+++ springDataJPAtutorial/src/test/java/com/example/springDataJPAtutorial/SpringDataJpAtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDataJPAtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpAtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
